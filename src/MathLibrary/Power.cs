79cac451c891f7ccd4032d2610029b93c771f879
Michal Rozsival <michalrozsival99@gmail.com>
2019-02-27 13:00:50 +0100

    add Power and Root class

---
+++ src/MathLibrary/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    class Power : IExpression
+    {
+
+
+
+        public double Evaluate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
