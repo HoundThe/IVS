e74f2825a6220c70027f8ff11c5e9e1a379635f7
Michal Rozsival <michalrozsival99@gmail.com>
2019-03-31 22:48:41 +0200

    Creating NLogharitmExpTest.cs which contain tests for extracted NLoghartimExp.cs which algorhitm has been separated from RootExp.cs

---
+++ src/MathLibraryTests/CompleteTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathLibraryTests
+{
+    [TestClass]
+    public class CompleteTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
